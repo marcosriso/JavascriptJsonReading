b76fd80560dddacaec785cfc37318725844c74d9
Marcos Riso <marcoseriso@gmail.com>
2016-09-15 15:06:45 -0300

    Completed

--- app/app.js
+++ app/app.js
@@ -4,18 +4,45 @@
 
 var app = angular.module('jsonReader', ['angular.filter']);
 
+app.filter("ucwords", function () {
+    return function (input){
+        if(input) { //when input is defined the apply filter
+            return (input + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+                return $1.toUpperCase();
+            });
+        }
+        return input;
+    }
+})
+
+
+
 app.controller('readBrands', function ($scope, $http){
 
     $scope.brands = [];
     $scope.users = [];
     $scope.interactions = [];
     $scope.table = {};
+    $scope.filterId = null;
+    $scope.filter = 'All Brands';
+
+    $scope.updateFilter = function(brand_id, name){
+        $scope.filterId = brand_id;
+        $scope.filter = name;
+        $scope.calculate();
+    };
+
+    $scope.reset = function(){
+        $scope.filterId = null;
+        $scope.filter = 'All Brands';
+        $scope.calculate();
+    }
 
     $scope.readBrandsInfo = function(){
         $http.get('data/frontend_data/brands.json').success(function(data) {
            $scope.brands = data;
         });
-    }
+    };
 
     $scope.calculate = function(){
         var userpromise = $http.get('data/frontend_data/users.json').success(function(data) {
@@ -31,11 +58,20 @@ app.controller('readBrands', function ($scope, $http){
                     function (payload2) {
 
                      var result =  $scope.interactions.reduce(function (res, obj) {
-                            if (!(obj.user in res)) {
-                                res.__array.push(res[obj.user] = obj);
-                                res[obj.user].count = 1;
+                            var rObj = {};
+                            if($scope.filterId != null){
+                                if(obj.brand == $scope.filterId){
+                                    rObj = obj;
+                                }
+                            }else{
+                                rObj = obj;
+                            }
+
+                            if (!(rObj.user in res)) {
+                                res.__array.push(res[rObj.user] = rObj);
+                                res[rObj.user].count = 1;
                             } else {
-                                res[obj.user].count += 1;
+                                res[rObj.user].count += 1;
                             }
                             return res;
                         }, {__array: []}).__array
@@ -58,17 +94,13 @@ app.controller('readBrands', function ($scope, $http){
                                 }
                             }
                         }
-
-
-                        console.log(optimized);
                         $scope.table = optimized;
                     }
-
 
                 )
             }
         );
 
-    }
+    };
 
 });
